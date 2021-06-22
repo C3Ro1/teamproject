7da3dc1ead0a9c0015050b88559251560bc1a20a
kaethy <86187953+kaethy@users.noreply.github.com>
2021-06-22 15:09:22 +0200

    Update ExperimentWorking (1).py

--- ExperimentWorking (1).py
+++ ExperimentWorking (1).py
@@ -4,11 +4,7 @@ Created on Sat Jun 19 15:51:06 2021
 
 @author: Kathi
 """
-print("Hello World")
 
-print("Hello World!")
-
-#Hello World
 
 
 import xlsxwriter
@@ -66,8 +62,9 @@ const = (const1 + const2) * 4
 print(const)
 
 #array with numbers of repetition (this and next position; 1 = second element)
-repetition = [(x % 9) + 1 for x in range(num_trials)]
+repetition = [(x % 9) for x in range(num_trials)]
 random.shuffle(repetition)
+print(repetition)
     
 
 #create workbook object for shapes file
@@ -94,8 +91,6 @@ for i in range(12):
     worksheetShapes.write(i + 24, 2, const[i + 24])
     worksheetShapes.write(i + 36, 2, const[i + 36])
 
-
-
 #create a workbook object for stimuli file
 workbook = xlsxwriter.Workbook(path + r"\stimuli.xlsx")
 worksheet = workbook.add_worksheet()
@@ -106,20 +101,137 @@ shapesheet = workbook_shapes.sheet_by_index(0)
 
 rowList = []
 
+
+
 def getFirst(element, i, constTrial):
-    idx = const.index(constTrial)
-    return idx
+    first3 = []
+    idx1 = const.index(constTrial)
+    idx2 = idx1 + 1 + const[idx1 + 1:].index(constTrial)
+    if(idx1 == i):
+        first3.append(listShapes[idx1 + 36])
+        first3.append(listShapes[idx2 + 36])
+        first3.append(listShapes[idx2 + 12])
+    elif(idx1 + 12 == i):
+        first3.append(listShapes[idx1 + 24])
+        first3.append(listShapes[idx2 + 24])
+        first3.append(listShapes[idx2])
+    elif(idx2 == i):
+        first3.append(listShapes[idx1 + 12])
+        first3.append(listShapes[idx1 + 36])
+        first3.append(listShapes[idx2 + 36])
+    elif(idx2 + 12 == i):
+        first3.append(listShapes[idx1])
+        first3.append(listShapes[idx1 + 24])
+        first3.append(listShapes[idx2 + 24])        
+    else:
+        print("Error")
+    #print(first3)
+    return random.choice(first3)
+
+#find next figure:
+#either partnerelement in the opposite lightness (j uneven)
+#or first Element of other pair in oppostie lightness (j even)
+def getNeighbor(element, i, constTrial, j):
+    nxt = "null"
+    #print(element)
+    if(j % 2 == 0):
+        idx1 = const.index(constTrial) #first element pair 1
+        idx2 = idx1 + 1 + const[idx1 + 1:].index(constTrial) #first element pair 2
+        if(listShapes[idx1] == element): #dark
+            nxt = listShapes[idx1 + 36]
+        elif(listShapes[idx1 + 12] == element): #light
+            nxt = listShapes[idx1 + 24] 
+        elif(listShapes[idx2] == element): #dark
+            nxt = listShapes[idx2 + 36]
+        elif(listShapes[idx2 + 12] == element): #light
+            nxt = listShapes[idx2 + 24]      
+        else:
+            print("Error")
+    else:
+        idx1 = const.index(constTrial) #first element pair 1
+        idx2 = idx1 + 1 + const[idx1 + 1:].index(constTrial) #first element pair 2
+        if(listShapes[idx1 + 24] == element): #dark
+            nxt = listShapes[idx2 + 12] #light
+        elif(listShapes[idx1 + 36] == element): #light
+            nxt = listShapes[idx2] #dark
+        elif(listShapes[idx2 + 24] == element):
+            nxt = listShapes[idx1 + 12]
+        elif(listShapes[idx2 + 36] == element):
+            nxt = listShapes[idx1]      
+        else:
+            print("Error")
+    return nxt
+
+#find partnerelement in the same color
+def getRepetition(element, i, constTrial, j):
+    if(j % 2 == 0):
+        idx1 = const.index(constTrial) #first element pair 1
+        idx2 = idx1 + 1 + const[idx1 + 1:].index(constTrial) #first element pair 2
+        if(listShapes[idx1] == element): #dark
+            nxt = listShapes[idx1 + 24]
+        elif(listShapes[idx1 + 12] == element): #light
+            nxt = listShapes[idx1 + 36] 
+        elif(listShapes[idx2] == element): #dark
+            nxt = listShapes[idx2 + 24]
+        elif(listShapes[idx2 + 12] == element): #light
+            nxt = listShapes[idx2 + 36]      
+        else:
+            print("Error")
+    else:
+        idx1 = const.index(constTrial) #first element pair 1
+        idx2 = idx1 + 1 + const[idx1 + 1:].index(constTrial) #first element pair 2
+        if(listShapes[idx1 + 24] == element):
+            nxt = listShapes[idx2]
+        elif(listShapes[idx1 + 36] == element):
+            nxt = listShapes[idx2 + 12]
+        elif(listShapes[idx2 + 24] == element):
+            nxt = listShapes[idx1]
+        elif(listShapes[idx2 + 36] == element):
+            nxt = listShapes[idx1 + 12]      
+        else:
+            print("Error")
+    return nxt
+
+listShapes = listDark[:12] + listLight[:12] + listDark[12:] + listLight[12:] 
+print(order)
+
+row = 0
+repidx = 0
 
 for i in order:
-    element = shapesheet.cell_value(i, 0)
-    print(element)
-    constTrial = shapesheet.cell_value(i, 1)
-    print("constTrial: " + str(constTrial))
-    #rowList.append(getFirst(element, i, constTrial))
+    element = listShapes[i]
+    #print(element)
+    constTrial = const[i]
+    #print(constTrial)
+    first = getFirst(element, i, constTrial)
+    rowList.append(first)
     rowList.append(element)
-    rep = repetition[i]
-    #getNeighbor(element)
-    print(getFirst(element, i, constTrial))
+    rep = repetition[repidx]
+    repidx = repidx + 1
+    print("rep:" + str(rep))
+    for j in range(9):
+        #if repetition is reached, add next element in same color
+        if(rep == j):
+            nxt = getRepetition(element, i, constTrial, j)
+            #print(element, nxt)
+            rowList.append(nxt)
+        #else add next element in different color
+        else:
+            nxt = getNeighbor(element, i, constTrial, j)
+            #print(element, nxt)
+            rowList.append(nxt)
+        #this new element is the basis for the following element
+        element = nxt
+    #print(rowList)
+    #write row in the excel table
+    for k in range(len(rowList)):
+        worksheet.write(row, k, rowList[k])
+    #count and delete old list for next iteration
+    row = row + 1
+    rowList = []   
+    
  
 
+    
 workbookStart.close()
+workbook.close()
